6cf01fd3fd056295e9a8fecf618b87f7916b10da
XSpiritWizardX <117311054+XSpiritWizardX@users.noreply.github.com>
2026-08-18 13:37:52 -0400

    Wire in party game upgrade layer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { installPartyUpgrade } from "./partyUpgrade.js";
 import "./styles.css";
+import "./partyUpgrade.css";
+
+installPartyUpgrade();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
